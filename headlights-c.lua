e20bfa8ab8dac78fcd3f6a481d732e8a93982b06
Christopher Flowers <cvflowers97@gmail.com>
2019-04-12 00:58:22 -0500

    Changed the while loop to only run once.

--- headlights-c.lua
+++ headlights-c.lua
@@ -1,12 +1,15 @@
 local player = GetPlayerPed(-1)
 local vehicle = GET_VEHICLE_PED_IS_USING(player)
+local doorOpen = IS_VEHICLE_DOOR_FULLY_OPEN(vehicle, 2)
 -- local backLeftDoorVehicles = {"ntypex"}
 
 Citizen.CreateThread(function()
 
     if (IS_PED_IN_VEHICLE(player, vehicle, true))
         if (IS_PED_IN_MODEL(player, "ntypex"))
-            SET_VEHICLE_DOOR_OPEN(vehicle, 0, false, true)
+            if (doorOpen == false)
+                SET_VEHICLE_DOOR_OPEN(vehicle, 2, false, true)
+            end
         end
     end
 
